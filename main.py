9ddeb540b484dda29ec7645bd3fc12eaa4964a9d
Elynejs <matthias.gomes03@gmail.com>
2020-03-06 19:46:11 +0100

    added csv data logging

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import keyboard as kb
-import time
+import time, csv, pandas
 
 start = time.time()
 t1 = time.time()
-end = t1 + 60
+end = t1 + 60 #change this number to change the duration(in seconds) of the mapping
+log = []
+i = 0
 
 while True:
     event = kb.read_event(False)
-    print(
-         f'{event} pressed {event.name} at {str(event.time - t1)[0:6]}')
+    print(f'{event} pressed {event.name} at {str(event.time - t1)[0:6]}')
     start = time.time()
-    if end <= start:
+    if 'down' in str(event):
+        Type = 'down'
+    else:
+        Type = 'up'
+    log.insert(i, {'Key': event.name, 'Time (s)': str(event.time - t1)[0:6], 'Type': Type})
+    i = i + 1
+    if end <= start: 
+        with open(f'log_file_{t1}.csv', "w") as file:
+            columns = ['Key', 'Time (s)', 'Type']
+            writer = csv.DictWriter(file, fieldnames=columns)
+            for j in range(0,i):
+                writer.writerow(log[j])
+            df = pandas.read_csv(f'log_file_{t1}.csv', index_col='Name')
+            print(df)
         break
