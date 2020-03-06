16c8152c12dee5afa542683dc54740bf94f496bd
Elynejs <matthias.gomes03@gmail.com>
2020-03-06 20:47:29 +0100

    .

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
 import keyboard as kb
 import time, csv, pandas
 
-start = time.time()
-t1 = time.time()
-end = t1 + 60 #change this number to change the duration(in seconds) of the mapping
-log = []
-i = 0
+start=time.time()
+t1=time.time()
+end=t1+10 #change this number to change the duration(in seconds) of the mapping
+log=[]
+i=0
 
 while True:
-    event = kb.read_event(False)
+    event=kb.read_event(False)
     print(f'{event} pressed {event.name} at {str(event.time - t1)[0:6]}')
-    start = time.time()
+    start=time.time()
     if 'down' in str(event):
-        Type = 'down'
+        Type='down'
     else:
-        Type = 'up'
-    log.insert(i, {'Key': event.name, 'Time (s)': str(event.time - t1)[0:6], 'Type': Type})
-    i = i + 1
-    if end <= start: 
-        with open(f'log_file_{t1}.csv', "w") as file:
-            columns = ['Key', 'Time (s)', 'Type']
-            writer = csv.DictWriter(file, fieldnames=columns)
+        Type='up'
+    log.insert(i,{'Key':event.name,'Time(s)':str(event.time-t1)[0:6],'Type':Type})
+    i=i+1
+    if end<=start: 
+        with open(f'./log/{t1}.csv',"w") as file:
+            columns=['Key','Time(s)','Type']
+            writer=csv.DictWriter(file,fieldnames=columns)
+
+            writer.writeheader()
             for j in range(0,i):
                 writer.writerow(log[j])
-            df = pandas.read_csv(f'log_file_{t1}.csv', index_col='Name')
+            
+            df = pandas.read_csv(f'./log/{t1}.csv')
             print(df)
         break
