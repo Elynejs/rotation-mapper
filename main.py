70c16d1103bbe782dcc80f5f147d54bdae8b3857
Elynejs <matthias.gomes03@gmail.com>
2020-03-07 18:55:58 +0100

    removed unecessary start variable

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import keyboard as kb
 import time, csv, pandas
 
-start=time.time()
 t1=time.time()
 filename = time.strftime("%Y-%m-%d_%Hh%Mm%Ss")
 end=t1+10 #change this number to change the duration(in seconds) of the mapping
@@ -11,11 +10,10 @@ i=0
 while True:
     event=kb.read_event(False)
     print(f'{event} pressed {event.name} at {str(event.time - t1)[:6]}')
-    start=time.time()
     Type='down'if('down'in str(event))else'up'
     log.insert(i,{'Key':event.name,'Time':str(event.time-t1)[:6],'Type':Type})
     i=i+1
-    if end<=start: 
+    if end<=event.time: 
         with open(f'./log/{filename}.csv',"w") as file:
             columns=['Key','Time','Type']
             writer=csv.DictWriter(file,fieldnames=columns)
