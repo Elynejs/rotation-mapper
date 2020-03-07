cbdd9949c89df513dae680d54b95e1670403b80d
Elynejs <matthias.gomes03@gmail.com>
2020-03-07 14:46:27 +0100

    changed ternary

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time, csv, pandas
 
 start=time.time()
 t1=time.time()
-end=t1+5 #change this number to change the duration(in seconds) of the mapping
+end=t1+60 #change this number to change the duration(in seconds) of the mapping
 log=[]
 i=0
 
@@ -11,17 +11,13 @@ while True:
     event=kb.read_event(False)
     print(f'{event} pressed {event.name} at {str(event.time - t1)[:6]}')
     start=time.time()
-    if 'down' in str(event):
-        Type='down'
-    else:
-        Type='up'
+    Type='down' if('down' in str(event))else 'up'
     log.insert(i,{'Key':event.name,'Time':str(event.time-t1)[:6],'Type':Type})
     i=i+1
     if end<=start: 
         with open(f'./log/{t1}.csv',"w") as file:
             columns=['Key','Time','Type']
             writer=csv.DictWriter(file,fieldnames=columns)
-
             writer.writeheader()
             for j in range(0,i):
                 writer.writerow(log[j])
