667e293525f674b27bc65997e14c9c8fdb065ed4
Elynejs <matthias.gomes03@gmail.com>
2020-03-07 18:53:48 +0100

    changed filename from epoch to current date

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import time, csv, pandas
 
 start=time.time()
 t1=time.time()
-end=t1+60 #change this number to change the duration(in seconds) of the mapping
+filename = time.strftime("%Y-%m-%d_%Hh%Mm%Ss")
+end=t1+10 #change this number to change the duration(in seconds) of the mapping
 log=[]
 i=0
 
@@ -11,16 +12,16 @@ while True:
     event=kb.read_event(False)
     print(f'{event} pressed {event.name} at {str(event.time - t1)[:6]}')
     start=time.time()
-    Type='down' if('down' in str(event))else 'up'
+    Type='down'if('down'in str(event))else'up'
     log.insert(i,{'Key':event.name,'Time':str(event.time-t1)[:6],'Type':Type})
     i=i+1
     if end<=start: 
-        with open(f'./log/{t1}.csv',"w") as file:
+        with open(f'./log/{filename}.csv',"w") as file:
             columns=['Key','Time','Type']
             writer=csv.DictWriter(file,fieldnames=columns)
             writer.writeheader()
             for j in range(0,i):
                 writer.writerow(log[j])
-        df = pandas.read_csv(f'./log/{t1}.csv', index_col='Key')
+        df = pandas.read_csv(f'./log/{filename}.csv', index_col='Key')
         print(df)
         break
