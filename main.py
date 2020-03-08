57383121ed0b26ce2fef4067f843506fdbcb08e5
Elynejs <matthias.gomes03@gmail.com>
2020-03-08 01:36:48 +0100

    heatmaps(bugged)

--- main.py
+++ main.py
@@ -1,25 +1,83 @@
 import keyboard as kb
 import time, csv, pandas
+from PIL import Image
 
 t1=time.time()
-filename = time.strftime("%Y-%m-%d_%Hh%Mm%Ss")
+dirname = time.strftime("%Y-%m-%d_%Hh%Mm%Ss")
 end=t1+10 #change this number to change the duration(in seconds) of the mapping
 log=[]
-i=0
+keys_pressed = []
+k_loc = {'1':(46,9,92,56),'59':(170,7,215,52),'60':(225,6,265,51),'61':(278,5,321,53),
+        '62':(330,6,375,51),'63':(384,4,427,53),'64':(436,5,481,53),'65':(490,4,532,52),
+        '66':(540,5,586,51),'67':(596,5,640,51),'68':(650,5,693,52),'87':(702,5,750,50),
+        '88':(755,5,805,50),'55':(845,5,895,50),'70':(900,5,950,50),'69':(950,5,995,50),
+        '41':(15,105,65,155),'2':(65,105,115,155),'3':(115,105,165,155),'4':(175,105,225,155),
+        '5':(225,105,275,155),'6':(280,105,325,155),'7':(335,105,380,155),'8':(385,105,430,155),
+        '9':(435,105,485,155),'10':(490,105,540,155),'11':(540,105,590,155),'12':(595,105,640,155),
+        '13':(650,105,695,155),'14':(700,105,800,155),'82':(840,105,890,155),'71':(895,105,940,155),
+        '73':(950,105,995,155),'69':(1035,105,1085,155),'53':(1090,105,1135,155),'55':(1145,105,1190,155),
+        '74':(1200,105,1245,155),'15':(20,165,75,210),'16':(80,165,125,210),'17':(130,165,180,210),
+        '18':(180,165,235,210),'19':(240,165,285,210),'20':(295,165,340,210),'21':(350,165,390,210),
+        '22':(400,165,445,210),'23':(450,165,500,210),'24':(505,165,550,210),'25':(560,165,605,210),
+        '26':(610,165,655,210),'27':(665,165,710,210),'28':(715,165,800,210),'83':(840,165,890,210),
+        '79':(895,165,940,210),'81':(950,165,995,210),'71':(1040,165,1085,210),'72':(1090,165,1140,210),
+        '73':(1145,165,1190,210),'78':(1200,165,1245,270),'58':(15,220,90,270),'30':(100,220,145,270),
+        '31':(150,220,200,270),'32':(205,220,255,270),'33':(260,220,305,270),'34':(315,220,360,270),
+        '35':(370,220,415,270),'36':(420,220,470,270),'37':(475,220,525,270),'38':(530,220,575,270),
+        '39':(580,220,630,270),'40':(635,220,680,270),'43':(690,220,735,270),'75':(1035,220,1085,270),
+        '76':(1090,220,1140,270),'77':(1145,220,1190,270),'42':(15,275,65,325),'86':(70,275,120,325),
+        '44':(130,275,175,325),'45':(180,275,230,325),'46':(235,275,285,325),'47':(290,275,340,325),
+        '48':(345,275,395,325),'49':(405,275,450,325),'50':(455,275,505,325),'51':(510,275,560,325),
+        '52':(570,275,615,325),'53':(625,275,670,325),'54':(680,275,800,325),'72':(895,275,945,325),
+        '79':(1040,275,1085,325),'80':(1090,275,1140,325),'81':(1145,275,1190,325),'28':(1195,275,1245,380),
+        '29':(20,335,75,385),'91':(90,335,135,385),'56':(150,335,195,385),'57':(200,335,615,385),
+        '541':(630,335,675,385),'92':(695,335,740,385),'93':(695,335,740,385),'29':(755,335,800,385),
+        '75':(845,335,890,385),'80':(895,335,940,385),'77':(950,335,995,385),'82':(1035,335,1140,385),
+        '83':(1145,335,1190,385)}
+
+
+def buildHeatmap():
+    global k_loc, keys_pressed
+    t = {}
+    for i in keys_pressed:
+        if i in t:
+            t[i] = t[i] + 1
+        else:
+            t[i] = 1
+    biggest = max(v for k, v in t.items())
+    heatmap = Image.open('./src/heat_gradient.png')
+    im = Image.open('./src/keyboard_image.png')
+    for (k, v) in t.items():
+        if(k not in k_loc):
+            continue
+        heatbox = ((int((v/biggest)*500)-1), 0, int((v/biggest)*500), 1)
+        heatRegion = heatmap.crop(heatbox)
+        heatColorInfo = heatRegion.getcolors()[0][1]
+        newImg = Image.new('RGB', (1250,404), heatColorInfo)
+        box = (k_loc[k][0], k_loc[k][1], k_loc[k][2], k_loc[k][3])
+        region = im.crop(box)
+        region = Image.blend(region, newImg, .5)
+        im.paste(region, box)
+    im.save(f'./log/{dirname}/heatmap.png')
+    im.show()
+    return True
+
+
 
 while True:
     event=kb.read_event(False)
-    print(f'{event} pressed {event.name} at {str(event.time - t1)[:6]}')
+    print(f'{event} pressed {event.name} at {str(event.time - t1)[:6]} with scan code {event.scan_code}')
     Type='down'if('down'in str(event))else'up'
-    log.insert(i,{'Key':event.name,'Time':str(event.time-t1)[:6],'Type':Type})
-    i=i+1
+    log.append({'Key':event.name,'Time':str(event.time-t1)[:5],'Type':Type,'Scan code':event.scan_code})
+    keys_pressed.append(event.scan_code)
     if end<=event.time: 
-        with open(f'./log/{filename}.csv',"w") as file:
-            columns=['Key','Time','Type']
+        with open(f'./log/{dirname}/log.csv',"w") as file:
+            columns=['Key','Time','Type','Scan code']
             writer=csv.DictWriter(file,fieldnames=columns)
             writer.writeheader()
-            for j in range(0,i):
-                writer.writerow(log[j])
-        df = pandas.read_csv(f'./log/{filename}.csv', index_col='Key')
+            for i in range(0,len(log)):
+                writer.writerow(log[i])
+        df = pandas.read_csv(f'./log/{dirname}/log.csv', index_col='Key')
         print(df)
+        buildHeatmap()
         break
